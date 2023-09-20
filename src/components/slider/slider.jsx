87d7b5d8632c14d9093184e25534dfb12314d5ae
ewudes <ewudes@gmail.com>
2023-09-20 14:12:08 +0300

    add: slider component

---
+++ src/components/slider/slider.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./slider.scss"
+
+const Slider = () => {
+  return (
+    <ul className="fade-slider">
+      <li className="fade-slider__item fade-slider__item--style-thirts fade-slider__item--visible">
+        <div className="fade-slider__text">
+          <h1 className="lined-text">
+            <span className="lined-text__word">три</span>
+            <span className="lined-text__word">футболки</span>
+            <span className="lined-text__word lined-text__word--color-yellow">по цене</span>
+            <span className="lined-text__word lined-text__word--color-yellow">двух</span>
+          </h1>
+        </div>
+      </li>
+      <li className="fade-slider__item fade-slider__item--style-games">
+        <div className="fade-slider__text">
+          <h1 className="lined-text">
+            <div className="lined-text__word">Любимые игры</div>
+            <div className="lined-text__word lined-text__word--color-yellow">на лучших футболках</div>
+          </h1>
+        </div>
+      </li>
+      <li className="fade-slider__item fade-slider__item--style-presents">
+        <div className="fade-slider__text">
+          <h1 className="lined-text">
+            <div className="lined-text__word">Подарки</div>
+            <div className="lined-text__word lined-text__word--color-yellow">для любимых</div>
+          </h1>
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+export default Slider;
