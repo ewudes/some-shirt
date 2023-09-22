8225a6f8e8469c794b4fb20ab0ad09f6e6d91e4f
ewudes <ewudes@gmail.com>
2023-09-22 18:27:02 +0300

    fix: app

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import {Routes, Route, BrowserRouter} from 'react-router-dom';
-import Main from '../../pages/main/main';
+import React from "react";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import Main from "../../pages/main/main";
+// import Product from "../../pages/product/product";
 
 const App = () => {
   return (
