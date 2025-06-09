80d49d8efc62b809b8b9961dec817eac143fd5a1
Ewudes <ewudes@gmail.com>
2025-06-09 21:06:38 +0300

    fix: header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <header className="header">
       <Link to="" className="logo">
-        <img src={require(`../../static/img/content/logos/logo-main.png`)} alt="lost shirt logo" className="logo__pic"></img>
+        <img src={require(`../../static/img/content/logos/logo-main.png`)} alt="lost shirt logo" width={100} height={100} className="logo__pic"></img>
       </Link>
       <Link to="mailto:lostinspace@mail.ru" className="mail">
         <div className="mail__title">По всем вопросам пишите на:</div>
