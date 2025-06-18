28a6b5cb1081e0c26e7d9c7d78ed28ba198e964e
Ewudes <ewudes@gmail.com>
2025-06-18 18:20:16 +0300

    fix: app component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import {Route, Routes} from "react-router-dom";
 import Main from "../../pages/main/main";
+import Product from "../../pages/product/product";
 
 import "../../index.scss";
-// import Product from "../../pages/product/product";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="/product" element={<Product />} />
     </Routes>
   );
 };
