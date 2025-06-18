be4b0791cdddeec124b62db30488bf36a6b0f8e8
Ewudes <ewudes@gmail.com>
2025-06-18 18:19:54 +0300

    fix: sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,40 +1,40 @@
 import React, {useState} from "react";
-import {Link} from "react-router-dom";
 import Menu from "../menu/menu";
 import IconsSVG from "../../static/img/icons-sprite.svg";
+import logo from "../../static/img/content/logos/logo-main.png";
 
 import "./sidebar.scss";
 
 const Sidebar = () => {
   const [isShowMenu, setIsShowMenu] = useState(false);
 
-  const handleShowMenu = () => {
-    setIsShowMenu((current) => !current);
-  };
+  const toggleMenu = () => setIsShowMenu((prev) => !prev);
 
   return (
-    <aside className={`${isShowMenu && `sidebar--opened`} sidebar`}>
-      <Link to="" className="sidebar__hamburger" onClick={handleShowMenu}>
+    <aside className={`sidebar ${isShowMenu && `sidebar--opened`}`}>
+      <button className="sidebar__hamburger" onClick={toggleMenu}>
         <svg className="sidebar__hamburger-icon">
-          <use xlinkHref={`${IconsSVG}#bars`}></use>
+          <use xlinkHref={`${IconsSVG}#bars`} />
         </svg>
-      </Link>
+      </button>
+
       <div className="sidebar__header">
         <div className="sidebar__logo">
           <img
-            src={require(`../../static/img/content/logos/logo-main.png`)}
+            src={logo}
             width={100}
             height={100}
-            alt=""
+            alt="Логотип"
             className="sidebar__logo-image"
-          ></img>
-          <Link to="" className="sidebar__close" onClick={handleShowMenu}>
+          />
+          <button className="sidebar__close" onClick={toggleMenu}>
             <svg className="sidebar__close-icon">
-              <use xlinkHref={`${IconsSVG}#cross`}></use>
+              <use xlinkHref={`${IconsSVG}#cross`} />
             </svg>
-          </Link>
+          </button>
         </div>
       </div>
+
       <Menu />
     </aside>
   );
