6cb09367e3df76078aa9f15c0bfc535d1a5f5fa0
Ewudes <ewudes@gmail.com>
2025-06-18 18:18:43 +0300

    fix: menu

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -3,31 +3,27 @@ import {Link} from "react-router-dom";
 
 import "./menu.scss";
 
+const menuItems = [
+  {label: `Детская коллекция`, to: ``},
+  {label: `Мужская коллекция`, to: ``},
+  {label: `Женская коллекция`, to: ``},
+  {label: `Популярные принты`, to: ``},
+  {label: `Новые принты`, to: ``},
+  {label: `Специальные предложения`, to: ``},
+  {label: `Скоро в продаже`, to: ``},
+];
+
 const Menu = () => {
   return (
     <nav className="menu">
       <ul className="menu__list">
-        <li className="menu__item">
-          <Link to="" className="menu__link">Детская коллекция</Link>
-        </li>
-        <li className="menu__item">
-          <Link to="" className="menu__link">Мужская коллекция</Link>
-        </li>
-        <li className="menu__item">
-          <Link to="" className="menu__link">Женская коллекция</Link>
-        </li>
-        <li className="menu__item">
-          <Link to="" className="menu__link">Популярные принты</Link>
-        </li>
-        <li className="menu__item">
-          <Link to="" className="menu__link">Новые принты</Link>
-        </li>
-        <li className="menu__item">
-          <Link to="" className="menu__link">Специальные предложения</Link>
-        </li>
-        <li className="menu__item">
-          <Link to="" className="menu__link">Скоро в продаже</Link>
-        </li>
+        {menuItems.map(({label, to}, index) => (
+          <li className="menu__item" key={index}>
+            <Link to={to} className="menu__link">
+              {label}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
