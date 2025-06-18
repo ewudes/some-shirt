a0ac96e34d9fe7f03c7631864f6690dd6eeaafde
Ewudes <ewudes@gmail.com>
2025-06-18 18:19:16 +0300

    fix: navigation

--- src/components/navigation/navigation.jsx
+++ src/components/navigation/navigation.jsx
@@ -3,88 +3,81 @@ import {Link} from "react-router-dom";
 
 import "./navigation.scss";
 
-const Navigation = () => {
-  return (
-    <nav className="site-menu">
-      <ul className="site-menu__sections">
-        <li className="site-menu__col">
-          <h3 className="site-menu__title">Информация</h3>
-          <ul className="site-menu__list">
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">О магазине</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Адреса магазинов</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Как сделать заказ?</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Обмен и возврат</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Отзывы</Link>
-            </li>
-          </ul>
-        </li>
-        <li className="site-menu__col">
-          <h3 className="site-menu__title">Футболки</h3>
-          <ul className="site-menu__list">
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Популярное</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Новинки</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Бестселлеры</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Специальные предложения</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Скоро в продаже</Link>
-            </li>
-          </ul>
-        </li>
-        <li className="site-menu__col">
-          <h3 className="site-menu__title">Коллекции</h3>
-          <ul className="site-menu__list">
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Детская коллекция</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Женская коллекция</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Мужская коллекция</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Лучшие принты 2018 года</Link>
-            </li>
-            <li className="site-menu__item">
-              <Link to="" className="site-menu__link">Лучшие принты 2019 года</Link>
-            </li>
-          </ul>
-        </li>
-        <li className="site-menu__col">
-          <h3 className="site-menu__title">Контактная информация</h3>
-          <div className="site-menu__text">
-            Санкт-Петербург, <br/>
-            ул.Жуковского, д.12
-          </div>
-          <div className="site-menu__text">
-            <Link to="tel:+78123157235" className="site-menu__text-row">
-              Телефон: 8(812)315-72-35
-            </Link>
-            <Link to="mailto:support@yourwebsite.com" className="site-menu__text-row">
-              Email: support@yourwebsite.com
-            </Link>
-          </div>
-        </li>
-      </ul>
-    </nav>
-  );
+const menuData = [
+  {
+    title: `Информация`,
+    links: [
+      `О магазине`,
+      `Адреса магазинов`,
+      `Как сделать заказ?`,
+      `Обмен и возврат`,
+      `Отзывы`,
+    ],
+  },
+  {
+    title: `Футболки`,
+    links: [
+      `Популярное`,
+      `Новинки`,
+      `Бестселлеры`,
+      `Специальные предложения`,
+      `Скоро в продаже`,
+    ],
+  },
+  {
+    title: `Коллекции`,
+    links: [
+      `Детская коллекция`,
+      `Женская коллекция`,
+      `Мужская коллекция`,
+      `Лучшие принты 2018 года`,
+      `Лучшие принты 2019 года`,
+    ],
+  },
+];
+
+const contactInfo = {
+  address: [`Санкт-Петербург,`, `ул.Жуковского, д.12`],
+  phone: {href: `tel:+78123157235`, text: `Телефон: 8(812)315-72-35`},
+  email: {href: `mailto:support@yourwebsite.com`, text: `Email: support@yourwebsite.com`},
 };
+
+const Navigation = () => (
+  <nav className="site-menu">
+    <ul className="site-menu__sections">
+      {menuData.map(({title, links}) => (
+        <li key={title} className="site-menu__col">
+          <h3 className="site-menu__title">{title}</h3>
+          <ul className="site-menu__list">
+            {links.map((text) => (
+              <li key={text} className="site-menu__item">
+                <Link to="" className="site-menu__link">{text}</Link>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+      <li className="site-menu__col">
+        <h3 className="site-menu__title">Контактная информация</h3>
+        <div className="site-menu__text">
+          {contactInfo.address.map((line, i) => (
+            <React.Fragment key={i}>
+              {line}
+              <br />
+            </React.Fragment>
+          ))}
+        </div>
+        <div className="site-menu__text">
+          <Link to={contactInfo.phone.href} className="site-menu__text-row">
+            {contactInfo.phone.text}
+          </Link>
+          <Link to={contactInfo.email.href} className="site-menu__text-row">
+            {contactInfo.email.text}
+          </Link>
+        </div>
+      </li>
+    </ul>
+  </nav>
+);
 
 export default Navigation;
