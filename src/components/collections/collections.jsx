cbeca0a9fa91a0ca0ce02165476c31ae6238d6ee
Ewudes <ewudes@gmail.com>
2025-06-18 18:18:14 +0300

    fix: collections

--- src/components/collections/collections.jsx
+++ src/components/collections/collections.jsx
@@ -3,27 +3,35 @@ import {Link} from "react-router-dom";
 
 import "./collections.scss";
 
+const collections = [
+  {
+    id: 1,
+    title: `Мужская коллекция`,
+    desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque.`,
+    typeClass: `collections__item--type-guys`,
+    link: ``,
+  },
+  {
+    id: 2,
+    title: `Женская коллекция`,
+    desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque.`,
+    typeClass: `collections__item--type-girls`,
+    link: ``,
+  },
+];
+
 const Collections = () => {
   return (
     <ul className="collections__list">
-      <li className="collections__item collections__item--type-guys">
-        <Link to="" className="collections__link">
-          <div className="collections__title">Мужская коллекция</div>
-          <div className="collections__desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque.
-          </div>
-          <div className="btn">Посмотреть</div>
-        </Link>
-      </li>
-      <li className="collections__item collections__item--type-girls">
-        <Link to="" className="collections__link">
-          <div className="collections__title">Женская коллекция</div>
-          <div className="collections__desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque.
-          </div>
-          <div className="btn">Посмотреть</div>
-        </Link>
-      </li>
+      {collections.map(({id, title, desc, typeClass, link}) => (
+        <li key={id} className={`collections__item ${typeClass}`}>
+          <Link to={link} className="collections__link">
+            <div className="collections__title">{title}</div>
+            <div className="collections__desc">{desc}</div>
+            <div className="btn">Посмотреть</div>
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 };
