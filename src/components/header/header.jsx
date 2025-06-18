2fc2cdb69b68a4ed52b438637e357f9ac7cf2627
Ewudes <ewudes@gmail.com>
2025-06-18 18:18:05 +0300

    fix: header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import logoMain from "../../static/img/content/logos/logo-main.png";
 
 import "./header.scss";
 
-const Header = () => {
-  return (
-    <header className="header">
-      <Link to="" className="header__logo">
-        <img
-          src={require(`../../static/img/content/logos/logo-main.png`)}
-          alt="some shirt logo"
-          width={100}
-          height={100}
-          className="header__pic"
-        ></img>
-      </Link>
-      <Link to="mailto:lostinspace@mail.ru" className="header__mail">
-        <div className="header__mail-title">По всем вопросам пишите на:</div>
-        <div className="header__mail-content">someshirt@gmail.com</div>
-      </Link>
-    </header>
-  );
-};
+const CONTACT_EMAIL = `someshirt@gmail.com`;
+const MAILTO_LINK = `mailto:lostinspace@mail.ru`;
+
+const Header = () => (
+  <header className="header">
+    <Link to="/" className="header__logo">
+      <img
+        src={logoMain}
+        alt="SomeShirt Logo"
+        width={100}
+        height={100}
+        className="header__pic"
+      />
+    </Link>
+    <a href={MAILTO_LINK} className="header__mail">
+      <div className="header__mail-title">По всем вопросам пишите на:</div>
+      <div className="header__mail-content">{CONTACT_EMAIL}</div>
+    </a>
+  </header>
+);
 
 export default Header;
