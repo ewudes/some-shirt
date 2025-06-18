6e9deccf3ba7b64873a291946d60437e589077c8
Ewudes <ewudes@gmail.com>
2025-06-18 18:18:33 +0300

    fix: filter

--- src/components/filter/filter.jsx
+++ src/components/filter/filter.jsx
@@ -3,27 +3,29 @@ import {Link} from "react-router-dom";
 
 import "./filter.scss";
 
+const filters = [
+  {id: `popular`, label: `Популярное`, isActive: true},
+  {id: `new`, label: `Новинки`},
+  {id: `bestsellers`, label: `Бестселлеры`},
+  {id: `specials`, label: `Специальные предложения`},
+  {id: `soon`, label: `Скоро в продаже`},
+];
+
 const Filter = () => {
   return (
     <ul className="filter">
-      <li className="filter__item filter__item--active">
-        <Link to="" data-filter="popular" className="filter__link">Популярное</Link>
-      </li>
-      <li className="filter__item">
-        <Link to="" data-filter="new" className="filter__link">Новинки</Link>
-      </li>
-      <li className="filter__item">
-        <Link to="" data-filter="bestsellers" className="filter__link">Бестселлеры</Link>
-      </li>
-      <li className="filter__item">
-        <Link to="" data-filter="specials" className="filter__link">Специальные предложения</Link>
-      </li>
-      <li className="filter__item">
-        <Link to="" data-filter="soon" className="filter__link">Скоро в продаже</Link>
-      </li>
+      {filters.map(({id, label, isActive}) => (
+        <li
+          key={id}
+          className={`filter__item${isActive && ` filter__item--active`}`}
+        >
+          <Link to="" data-filter={id} className="filter__link">
+            {label}
+          </Link>
+        </li>
+      ))}
     </ul>
   );
 };
 
 export default Filter;
-
