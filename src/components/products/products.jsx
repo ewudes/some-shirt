ab8da0434228735890a4f4d34d18fb22bdb85933
Ewudes <ewudes@gmail.com>
2025-06-18 18:19:07 +0300

    fix: products

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -1,97 +1,86 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import PropTypes from "prop-types";
 
 import "./products.scss";
 
-const Products = () => {
-  return (
-    <ul className="products__list">
-      <li className="products__item new bestsellers">
-        <div className="products__price">
-          <div className="products__price-amount">1 800</div>
-        </div>
-        <div className="products__content">
-          <img src={require(`../../static/img/tshirts/tshirt_01.png`)} alt="bo-jack tshirt" className="products__img"></img>
-          <div className="products__desc">
-            <div className="products__desc-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </div>
-            <Link to="" className="btn">Подробнее</Link>
-          </div>
-        </div>
-      </li>
-      <li className="products__item new specials soon">
-        <div className="products__price">
-          <div className="products__price-amount">1 800</div>
-        </div>
-        <div className="products__content">
-          <img src={require(`../../static/img/tshirts/tshirt_02.png`)} alt="bo-jack tshirt" className="products__img"></img>
-          <div className="products__desc">
-            <div className="products__desc-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </div>
-            <Link to="" className="btn">Подробнее</Link>
-          </div>
-        </div>
-      </li>
-      <li className="products__item products__item--color-black products__item--size-double soon bestsellers">
-        <div className="products__price">
-          <div className="products__price-amount">1 800</div>
-        </div>
-        <div className="products__content">
-          <img src={require(`../../static/img/tshirts/tshirt_03.png`)} alt="bo-jack tshirt" className="products__img"></img>
-          <div className="products__desc">
-            <div className="products__desc-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </div>
-            <Link to="" className="btn">Подробнее</Link>
-          </div>
-        </div>
-      </li>
-      <li className="products__item products__item--color-black products__item--size-double specials">
-        <div className="products__price">
-          <div className="products__price-amount">1 800</div>
-        </div>
-        <div className="products__content">
-          <img src={require(`../../static/img/tshirts/tshirt_04.png`)} alt="bo-jack tshirt" className="products__img"></img>
-          <div className="products__desc">
-            <div className="products__desc-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </div>
-            <Link to="" className="btn">Подробнее</Link>
-          </div>
-        </div>
-      </li>
-      <li className="products__item specials popular bestsellers">
-        <div className="products__price">
-          <div className="products__price-amount">1 800</div>
-        </div>
-        <div className="products__content">
-          <img src={require(`../../static/img/tshirts/tshirt_05.png`)} alt="bo-jack tshirt" className="products__img"></img>
-          <div className="products__desc">
-            <div className="products__desc-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </div>
-            <Link to="" className="btn">Подробнее</Link>
-          </div>
-        </div>
-      </li>
-      <li className="products__item soon popular">
-        <div className="products__price">
-          <div className="products__price-amount">1 800</div>
-        </div>
-        <div className="products__content">
-          <img src={require(`../../static/img/tshirts/tshirt_06.png`)} alt="bo-jack tshirt" className="products__img"></img>
-          <div className="products__desc">
-            <div className="products__desc-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </div>
-            <Link to="" className="btn">Подробнее</Link>
-          </div>
-        </div>
-      </li>
-    </ul>
-  );
+const productData = [
+  {
+    id: 1,
+    price: 1800,
+    image: `tshirt_01.png`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    className: `new bestsellers`,
+  },
+  {
+    id: 2,
+    price: 1800,
+    image: `tshirt_02.png`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    className: `new specials soon`,
+  },
+  {
+    id: 3,
+    price: 1800,
+    image: `tshirt_03.png`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    className: `products__item--color-black products__item--size-double soon bestsellers`,
+  },
+  {
+    id: 4,
+    price: 1800,
+    image: `tshirt_04.png`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    className: `products__item--color-black products__item--size-double specials`,
+  },
+  {
+    id: 5,
+    price: 1800,
+    image: `tshirt_05.png`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    className: `specials popular bestsellers`,
+  },
+  {
+    id: 6,
+    price: 1800,
+    image: `tshirt_06.png`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
+    className: `soon popular`,
+  },
+];
+
+const ProductItem = ({price, image, desc, className}) => (
+  <li className={`products__item ${className}`}>
+    <div className="products__price">
+      <div className="products__price-amount">{price.toLocaleString(`ru-RU`)}</div>
+    </div>
+    <div className="products__content">
+      <img
+        src={require(`../../static/img/tshirts/${image}`)}
+        alt="bo-jack tshirt"
+        className="products__img"
+      />
+      <div className="products__desc">
+        <div className="products__desc-text">{desc}</div>
+        <Link to="" className="btn">Подробнее</Link>
+      </div>
+    </div>
+  </li>
+);
+
+const Products = () => (
+  <ul className="products__list">
+    {productData.map((product) => (
+      <ProductItem key={product.id} {...product} />
+    ))}
+  </ul>
+);
+
+ProductItem.propTypes = {
+  price: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired,
+  className: PropTypes.string,
 };
 
 export default Products;
