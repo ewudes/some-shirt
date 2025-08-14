bed3096164bdc941dbe0db2fa2c9071cdc65ea04
Ewudes <ewudes@gmail.com>
2025-08-14 17:30:40 +0300

    add: scroll to products section

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -6,19 +6,29 @@ import IconsSVG from "../../static/img/icons-sprite.svg";
 
 import "./hero.scss";
 
-const Hero = () => (
-  <section className="hero">
-    <Header />
-    <div className="hero__content">
-      <Slider />
-    </div>
-    <Sidebar />
-    <a href="#" className="hero__arrow">
-      <svg className="hero__arrow-icon">
-        <use xlinkHref={`${IconsSVG}#arrow`} />
-      </svg>
-    </a>
-  </section>
-);
+const Hero = () => {
+  const scrollToProduct = (e) => {
+    e.preventDefault();
+    const productSection = document.getElementById(`products`);
+    if (productSection) {
+      productSection.scrollIntoView({behavior: `smooth`});
+    }
+  };
+
+  return (
+    <section className="hero">
+      <Header />
+      <div className="hero__content">
+        <Slider />
+      </div>
+      <Sidebar />
+      <a href="#" className="hero__arrow" onClick={scrollToProduct}>
+        <svg className="hero__arrow-icon">
+          <use xlinkHref={`${IconsSVG}#arrow`} />
+        </svg>
+      </a>
+    </section>
+  );
+};
 
 export default Hero;
