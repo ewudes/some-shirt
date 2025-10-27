694ae8bc3d47f4419417c543cff40116fcfa9056
ewudes <ewudes@gmail.com>
2025-10-27 16:17:30 +0300

    fix: sidebar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import {useState, useEffect} from "react";
 import Menu from "../menu/menu";
 import IconsSVG from "../../static/img/icons-sprite.svg";
 
